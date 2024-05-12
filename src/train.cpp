8fc3c9c53f47b555713451174cddd1a643839fb7
yegor52 <yegorbeat2005@gmail.com>
2024-05-13 02:05:20 +0300

    ADS-8

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,45 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+Train::Train() : countOp(0), first(nullptr) {}
+
+void Train::addCage(bool light) {
+    if (first != nullptr) {
+        Cage* next = first->next;
+        first->next = new Cage;
+        first->next->prev = first;
+        first = first->next;
+        next->prev = first;
+        first->next = next;
+        first->light = light;
+    } else {
+        first = new Cage;
+        first->light = light;
+        first->prev = first;
+        first->next = first;
+    }
+}
+
+int Train::getLength() {
+    int length = 0;
+    Cage* currentWagon = first;
+
+    if (!currentWagon->light) {
+        currentWagon->light = true;
+    }
+
+    for (;;) {
+        if (!currentWagon->next->light) {
+            currentWagon = currentWagon->next;
+            countOp++;
+            length++;
+            continue;
+        }
+        break;
+    }
+
+    return length;
+}
+
+int Train::getOpCount() {
+    return countOp;
+}
